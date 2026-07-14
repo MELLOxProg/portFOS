f6ee0e6dad6f8ee91f6b12b755db58a47f2eb271
Nirav Kandira <niravkandira@yahoo.in>
2026-07-15 02:10:01 +0530

    Add Nav component with DateTime display and update styles;

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './navbar.scss'
+import DateTime from './DateTime'
+
+const Navbar = () => {
+  return (
+<nav>
+    <div className="left">
+        <div className="comp-icon">
+            <img src="./navbar-icons/mac.svg" alt="" />
+        </div>
+        <div className="nav-item">
+            <p>Nirav Kandira</p>
+        </div>
+        <div className="nav-item">
+            <p>File</p>
+        </div>
+        <div className="nav-item">
+            <p>Window</p>
+        </div>
+        <div className="nav-item">
+            <p>Terminal</p>
+        </div>
+    </div>
+    <div className="right">
+        <div className="nav-icon">
+            <img src="/navbar-icons/wifi.svg" alt="" />
+        </div>
+             <div className="nav-item">
+            <DateTime />
+        </div>
+    </div>
+</nav>
+  )
+}
+
+export default Navbar
